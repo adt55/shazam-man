1dbdb36d5c0942d7f97d6734948522f432643776
adt55 <78002456+adt55@users.noreply.github.com>
2021-01-26 19:49:38 -0500

    First check in of Initial Objects
    Describing songs, albums, releases, playlists, etc.

---
+++ ShazamExample1/ShazamExample1/Activity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShazamExample1
+{
+    enum ActivityType
+    {
+        Calm,
+        Moderate,
+        Strenuous,
+        Meditation,
+        Study,
+        Driving,
+        Chores,
+        Sad,
+        Hype,
+        InMyFeelings,
+        InMyShower
+    }
+    class Activity
+    {
+        public string ActivityName;
+        public ActivityType thisActivityType;
+    }
+}
